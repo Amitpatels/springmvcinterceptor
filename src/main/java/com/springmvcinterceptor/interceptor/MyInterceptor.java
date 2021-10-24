bd873bfc025b53b47a81570c6eed9df02c74c0aa
Amit812 <patelgithub812@gmail.com>
2021-10-24 18:47:28 +0530

    first comit

---
+++ src/main/java/com/springmvcinterceptor/interceptor/MyInterceptor.java
@@ -0,0 +1,43 @@
+package com.springmvcinterceptor.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)	throws Exception {
+		
+		System.out.println("This is pre handler ...");
+		
+		String name = request.getParameter("user");
+		if(name.startsWith("p")) {
+			response.setContentType("text/html");
+			response.getWriter().println("<h1>Invalid name should not start with p</h1>");
+			return false;
+		}
+		
+		return true;	
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("This is post handler ...");
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("This is after completation...");
+		super.afterCompletion(request, response, handler, ex);
+	}
+	
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@page isELIgnored="false" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Dark Web</title>
+</head>
+<body>
+
+	<h1>Welcome ${name} into the dark web</h1>
+</body>
+</html>
